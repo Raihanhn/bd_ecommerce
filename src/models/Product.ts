eb6f1d2460367aaac9dc04c190af75e717b88ef4
Mohammad Raihan <108555328+Raihanhn@users.noreply.github.com>
2025-10-14 22:10:01 +0600

    product model added

---
+++ src/models/Product.ts
@@ -0,0 +1,16 @@
+import mongoose, { Schema, models } from "mongoose";
+
+const productSchema = new Schema({
+  name: { type: String, required: true },
+  slug: { type: String, required: true, unique: true },
+  description: { type: String },
+  price: { type: Number, required: true, default: 0 },
+  sku: { type: String },
+  quantity: { type: Number, default: 0 },
+  category: { type: Schema.Types.ObjectId, ref: "Category" },
+  images: [{ type: String }], // array of image URLs
+  isVisible: { type: Boolean, default: true },
+  attributes: { type: Object }, // e.g. color, size
+}, { timestamps: true });
+
+export default models.Product || mongoose.model("Product", productSchema);
