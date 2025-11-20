4e1c875450874312e5b99c73754dcda3ca120e32
Mohammad Raihan <108555328+Raihanhn@users.noreply.github.com>
2025-11-21 04:51:28 +0600

    footer fixed to bottom position

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,24 +2,31 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import Navbar from "@/components/Navbar";
-import { useRouter } from "next/router";
 import Footer from "@/components/Footer";
+import { useRouter } from "next/router";
 
-export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   const router = useRouter();
 
-  // Define routes where Navbar should be hidden
+  // Routes where Navbar should be hidden
   const hideNavbarRoutes = ["/"];
-
   const shouldHideNavbar = hideNavbarRoutes.includes(router.pathname);
 
   return (
     <SessionProvider session={session}>
-      {!shouldHideNavbar && <Navbar />}
-      <main className={!shouldHideNavbar ? "" : ""}>
-        <Component {...pageProps} />
-      </main>
-       <Footer />
+      <div className="flex flex-col min-h-screen">
+        {!shouldHideNavbar && <Navbar />}
+
+        {/* Make main take all remaining space */}
+        <main className="flex-1">
+          <Component {...pageProps} />
+        </main>
+
+        <Footer />
+      </div>
     </SessionProvider>
   );
 }
